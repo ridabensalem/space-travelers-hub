be6650a4b968dc4121f4ae1715ac99360b32e0c3
Nezerwa <eligrand2000@gmail.com>
2023-03-15 19:08:36 +0200

    Update Dragons component to render loading and error states while fetching data

--- src/components/dragons.jsx
+++ src/components/dragons.jsx
@@ -3,41 +3,54 @@ import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import fetchDragon from './API/apiDragons';
 
-function Dragon({
-  // eslint-disable-next-line react/prop-types
-  name, type, image,
-}) {
+function Dragon({ name, type, image }) {
   return (
-
     <div>
       <h1>{name}</h1>
       <p>{type}</p>
-      <p>{image}</p>
+      <img src={image} alt={name} />
     </div>
   );
 }
+
 Dragon.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
 };
+
 function Dragons() {
-  const dragonsArr = useSelector((state) => state.dragonReducer.dragonStore);
+  const { dragons, status, error } = useSelector(
+    (state) => state.dragonReducer,
+  );
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(fetchDragon());
   }, [dispatch]);
 
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
+
   return (
     <div>
-      {dragonsArr.map((dragon) => (
+      {dragons.map((dragon) => (
         <Dragon
           key={dragon.id}
-          id={dragon.id}
           name={dragon.name}
           type={dragon.type}
-          image={dragon.flickr_images}
+          image={dragon.flickr_images[0]}
         />
-
       ))}
     </div>
   );
