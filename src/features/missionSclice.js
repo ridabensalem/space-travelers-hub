e8a47c16c4a9d6cc6a32c87b1bc7229e3d6e642c
ridabensalem <ridasbnesalmas@gmail.com>
2023-03-18 10:07:47 +0100

    Remove the commented code

--- src/features/missionSclice.js
+++ src/features/missionSclice.js
@@ -52,24 +52,3 @@ export const missionSlice = createSlice({
 export const { reserveMission } = missionSlice.actions;
 export const { leaveMission } = missionSlice.actions;
 export default missionSlice.reducer;
-// export const missionSlice = createSlice({
-//   name: 'mission',
-//   initialState,
-//   reducers: {},
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(fetchMission.pending, (state) => {
-//         state.status = 'loading';
-//       })
-//       .addCase(fetchMission.fulfilled, (state, action) => {
-//         state.status = 'succeeded';
-//         state.missionStore = action.payload;
-//       })
-//       .addCase(fetchMission.rejected, (state, action) => {
-//         state.status = 'failed';
-//         state.error = action.error.message;
-//       });
-//   },
-// });
-
-// export default missionSlice.reducer;
